6e60429542be2078ae83091114d680c2b541e9de
antonnifo <antonnifo@live.com>
2018-12-01 11:53:06 +0300

    [finishes #162297565] polish config n tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask,Blueprint
-# from flask_restful import Api, Resource
+from app.api.v1 import version1 as v1
+from app.instance.config import APP_CONFIG
 
-#local imports
-# from .api.v1.view import RedFlags, RedFlag
-from .api.v1 import version1 as v1
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(APP_CONFIG[config_name])
+    app.config.from_pyfile('config.py', silent=True)
 
-def create_app():
-  
-    app = Flask(__name__)
     app.register_blueprint(v1)
-    return app 
+    
+    return app
+    
--- run.py
+++ run.py
@@ -1,6 +1,4 @@
+import os
 from app import create_app
 
-app =create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app = create_app(os.getenv("FLASK_CONF"))
