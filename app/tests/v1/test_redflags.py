e1d84ff084caa8f684bd4ed8f7a618934476db24
antonnifo <antonnifo@live.com>
2018-12-06 05:45:24 +0300

    [finish #162410878] post n test users

--- app/tests/v1/test_redflags.py
+++ app/tests/v1/test_redflags.py
@@ -1,3 +1,4 @@
+"""Tests for redflags run with pytest"""
 import json
 import unittest
 
@@ -5,72 +6,89 @@ from ... import create_app
 
 
 class RedFlagTestCase(unittest.TestCase):
-    
     """
     This class represents the redflag test cases
     """
+
     def setUp(self):
         APP = create_app("testing")
         self.app = APP.test_client()
         self.redflag = {
-            "id": 1,
-            "createdOn" : "Fri, 2 March 2018 12:08:13 GMT",
-            "createdBy" : "Antonnifo",
-            'type' : 'red-flags',
-            "location" : "Kahawa",
-            "status" : "Under investigation",
-            "images" : "",
-            "videos" : "",
-            "title" : "NYS scandal",
-            "comment" : "Hello Andela."
+            "createdOn": "Fri, 2 March 2018 12:08:13 GMT",
+            "createdBy": "Antonnifo",
+            'type': 'red-flag',
+            "location": "Kahawa",
+            "status": "Under investigation",
+            "images": "",
+            "videos": "",
+            "title": "NYS scandal",
+            "comment": "Hello Andela."
         }
 
     def test_get_all_redflags(self):
+        """method to test get all"""
         response = self.app.get("/api/v1/red-flags")
         self.assertEqual(response.status_code, 200)
-        
 
     def test_post_redflag(self):
-        response = self.app.post("/api/v1/red-flags", headers={'Content-Type': 'application/json'},
-         data = json.dumps(self.redflag))
+        """method to test posting of a redflag"""
+        response = self.app.post(
+            "/api/v1/red-flags", headers={'Content-Type': 'application/json'}, data=json.dumps(self.redflag))
         result = json.loads(response.data)
         self.assertEqual(response.status_code, 201)
-        self.assertIn('Created red-flag record',str(result))
+        self.assertIn('Created red-flag record', str(result))
 
     def test_get_specific_redflag(self):
-        self.app.post("/api/v1/red-flags", headers={'Content-Type': 'application/json'},
-         data = json.dumps(self.redflag))
+        """method to test if one can get a specific redflag"""
+        self.app.post("/api/v1/red-flags",
+                      headers={'Content-Type': 'application/json'}, data=json.dumps(self.redflag))
         response = self.app.get("/api/v1/red-flags/1")
         json.loads(response.data)
-        self.assertEqual(response.status_code, 200)      
+        self.assertEqual(response.status_code, 200)
 
     def test_delete_specific_redflag(self):
-        self.app.post("/api/v1/red-flags", headers={'Content-Type': 'application/json'},
-         data = json.dumps(self.redflag))
+        """method to test if one can delete a redflag"""
+        self.app.post("/api/v1/red-flags",
+                      headers={'Content-Type': 'application/json'}, data=json.dumps(self.redflag))
         response = self.app.delete("/api/v1/red-flags/1")
         result = json.loads(response.data)
         self.assertEqual(response.status_code, 200)
-        self.assertIn('red-flag record has been deleted',str(result))
+        self.assertIn('red-flag record has been deleted', str(result))
 
     def test_update_location_of_specific_redflag(self):
-        self.app.post("/api/v1/red-flags/1/location", headers={'Content-Type': 'application/json'},
-         data = json.dumps(self.redflag))
-        response = self.app.patch("/api/v1/red-flags/1/location", headers={'Content-Type': 'application/json'},
-         data = json.dumps({"location" : "Kayole"}))
+        """method to test edit of location"""
+        self.app.post("/api/v1/red-flags/1/location",
+                      headers={'Content-Type': 'application/json'}, data=json.dumps(self.redflag))
+        response = self.app.patch("/api/v1/red-flags/1/location", headers={
+                                  'Content-Type': 'application/json'}, data=json.dumps({"location": "Kayole"}))
         result = json.loads(response.data)
-        self.assertEqual(response.status_code, 201)
-        self.assertIn("Updated red-flag record's location",str(result))
-            
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Updated red-flag record's location", str(result))
 
     def test_update_comment_of_specific_redflag(self):
-        self.app.post("/api/v1/red-flags/1/comment", headers={'Content-Type': 'application/json'},
-         data = json.dumps(self.redflag))
+        """method to test edit of comment"""
+        self.app.post("/api/v1/red-flags/1/comment",
+                      headers={'Content-Type': 'application/json'}, data=json.dumps(self.redflag))
         response = self.app.patch("/api/v1/red-flags/1/comment", headers={'Content-Type': 'application/json'},
-         data = json.dumps({"comment" : "hello cohart 35"}))
+                                  data=json.dumps({"comment": "hello cohart 35"}))
         result = json.loads(response.data)
-        self.assertEqual(response.status_code, 201) 
+        self.assertEqual(response.status_code, 200)
         self.assertIn("Updated red-flag record's comment",
-        str(result))              
+                      str(result))
 
-if __name__ == "__main__": 
-    unittest.main() 
+    def test_redflag_not_found(self):
+        """Test a redflag not found"""
+        response = self.app.get("/api/v1/red-flags/100")
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            result['error'], "red flag does not exit")
+
+    def test_wrong_comment_key(self):
+        """Test wrong comment key used in redflag"""
+        response = self.app.patch("/api/v1/red-flags/1/comment", headers={'Content-Type': 'application/json'},
+                                  data=json.dumps({"comment1": "hello pac"}))
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(
+            result['error'], "KeyError Red-flag's comment not updated")
