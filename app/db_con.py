76e7f1c7ef94c6adaa395f5e6cdab350d1112edb
antonnifo <antonnifo@live.com>
2018-12-28 15:48:20 +0300

    [chore #162831438] refactor superuser n connection]

--- app/db_con.py
+++ app/db_con.py
@@ -3,28 +3,20 @@ import os
 
 import psycopg2 as p
 import psycopg2.extras
+from werkzeug.security import generate_password_hash
 
-test_url = "dbname='test_ireporter' host='localhost' port='5432' user='postgres' password='bssc4344'"
-url = os.getenv('DATABASE_URL',test_url )
-
+DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL_TEST = os.getenv('DATABASE_URL_TEST')
 
 def connection(url):
-    con = p.connect(url)
-    print("connecting to db...connected")
+    con = p.connect(DATABASE_URL)
     return con
-
-
-def cursor(url):
-    con = connection(url)
-    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    return cur
-
 
 def create_tables():
     '''A database cursor is an object that points to a
     place in the database where we want to create, read,
     update, or delete data.'''
-    con = connection(url)
+    con = connection(DATABASE_URL)
     curr = con.cursor()
     queries = tables()
 
@@ -34,7 +26,7 @@ def create_tables():
 
 
 def destroy_tables():
-    con = connection(test_url)
+    con = connection(DATABASE_URL_TEST)
     curr = con.cursor()
     users = "DROP TABLE IF EXISTS users CASCADE"
     incidents = "DROP TABLE IF EXISTS incidents CASCADE"
@@ -43,7 +35,7 @@ def destroy_tables():
         for query in queries:
             curr.execute(query)
         con.commit()
-        print('Destroying tables...Done ')
+        print('Destroying test tables...Done ')
     except:
         print("Failed to Destroy tables")
 
@@ -51,13 +43,14 @@ def destroy_tables():
 def tables():
     tbl1 = """CREATE TABLE IF NOT EXISTS incidents (
 	    incidents_id serial PRIMARY KEY NOT NULL,
+        createdOn TIMESTAMP,
 	    type character (64) NOT NULL,
 	    title  character (64) NOT NULL,
 	    location character(64) NOT NULL,
+        Images VARCHAR(500) NULL,
 	    status character (64) NOT NULL,
 	    comment character (1000) NOT NULL,
-        createdBy INT NOT NULL,
-	    createdON timestamp with time zone DEFAULT ('now'::text)::date NOT NULL
+        createdBy INT NOT NULL
 	    )"""
 
     tbl2 = """create table IF NOT EXISTS users (
@@ -67,7 +60,7 @@ def tables():
      email varchar(50) NOT NULL UNIQUE,
      phone varchar(11),
      isAdmin boolean NOT NULL,  
-     date_created timestamp with time zone DEFAULT ('now'::text)::date NOT NULL, 
+     registered TIMESTAMP, 
      password varchar(500) NOT NULL
      )"""
 
@@ -75,71 +68,24 @@ def tables():
     return queries
 
 
-def test_user_admin():
+def super_user():
+    password = generate_password_hash("hello123")
     user_admin = {
         "first_name": "john",
         "last_name": "doe",
         "email": "johndoe@example.com",
-        "phone": "0708767676",
+        "phone": "0707741793",
         "isAdmin": True,
-        "date_created": "Thu, 13 Dec 2018 21:00:00 GMT",
-        "password": "pbkdf2:sha256:50000$OwVe1ERR$ccdcf27b466c87f3fdf581183693536651e0dd8094c68d281b224375addcba3d"
-
-
+        "registered": "Thu, 13 Dec 2018 21:00:00 GMT",
+        "password": password
     }
-    query = """INSERT INTO users (first_name,last_name,email,phone,password,isAdmin,date_created) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}');""".format(
-        user_admin['first_name'], user_admin['last_name'], user_admin['email'], user_admin['phone'], user_admin['password'], user_admin['isAdmin'], user_admin['date_created'])
-    conn = connection(url)
+    query = """INSERT INTO users (first_name,last_name,email,phone,password,isAdmin,registered) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}');""".format(
+        user_admin['first_name'], user_admin['last_name'], user_admin['email'], user_admin['phone'], user_admin['password'], user_admin['isAdmin'], user_admin['registered'])
+    conn = connection(DATABASE_URL)
     cursor = conn.cursor()
     try:
         cursor.execute(query)
         conn.commit()
-        print('super admin created')
+        print('super user created')
     except:
         return "user already exists"
-
-
-def test_intervention():
-    incident = {
-        "createdBy": 1,
-        "type": "intervention",
-        "location": "66, 12",
-        "status": "draft",
-        "title": "NYS scandal",
-        "comment": "act soon",
-        "createdon": "Thu, 13 Dec 2018 14:31:20 GMT"
-    }
-    query = """INSERT INTO incidents (createdBy,type,location,status,title,comment,createdon) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}');""".format(
-        incident['createdBy'], incident['type'], incident['location'], incident['status'], incident['title'], incident['comment'], incident['createdon'])
-    conn = connection(url)
-    cursor = conn.cursor()
-    try:
-        cursor.execute(query)
-        conn.commit()
-        print('test intervention created')
-    except:
-        return "test intervention already exists"
-
-
-def test_redflag():
-    incident = {
-        "createdBy": 1,
-        "type": "redflag",
-        "location": "66, 13",
-        "status": "draft",
-        "title": "NYS scandal",
-        "comment": "act soon",
-        "createdon": "Thu, 13 Dec 2018 14:31:20 GMT"
-
-
-    }
-    query = """INSERT INTO incidents (createdBy,type,location,status,title,comment,createdon) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}');""".format(
-        incident['createdBy'], incident['type'], incident['location'], incident['status'], incident['title'], incident['comment'], incident['createdon'])
-    conn = connection(url)
-    cursor = conn.cursor()
-    try:
-        cursor.execute(query)
-        conn.commit()
-        print('test redflag created')
-    except:
-        return "test redflag already exists"
